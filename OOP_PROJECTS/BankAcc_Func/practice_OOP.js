12cdd50606b99f93225ab3fb6d22406a3fa5eaa9
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-09-13 07:12:41 -0400

    sex orgy class

--- OOP_PROJECTS/BankAcc_Func/practice_OOP.js
+++ OOP_PROJECTS/BankAcc_Func/practice_OOP.js
@@ -6,3 +6,32 @@ const myEmployees  = {
 
 
 console.log(myEmployees.designer);
+
+
+class Orgy{
+
+    constructor(name, age, gender, skills){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.skills = skills;
+    }
+    
+    pplGangbang(amountOfPpl){
+        if(amountOfPpl < 5){
+            console.log(`participants should be no less than 5`);
+        }
+        else{
+            console.log(`orgy can be possible!`);
+        }
+    }
+}
+
+
+const person_1 = new Orgy("chad", 24, "male", "good in doggyStyle");
+
+const person_2 = new Orgy("bonnie", 19, "whore-female", "spreading legs");
+
+
+console.log(person_1.skills);
+console.log(person_2.name);
