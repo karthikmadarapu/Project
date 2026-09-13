cfd2e91bea3b36972d35949c5f4c96011def09f8
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-09-13 07:42:36 -0400

    takeDamage method executed

--- OOP_PROJECTS/BankAcc_Func/practice_OOP.js
+++ OOP_PROJECTS/BankAcc_Func/practice_OOP.js
@@ -35,3 +35,35 @@ const person_2 = new Orgy("bonnie", 19, "whore-female", "spreading legs");
 
 console.log(person_1.skills);
 console.log(person_2.name);
+
+
+class Character{
+     
+    constructor(name, heroClass, health){
+        this.name = name;
+        this.heroClass = heroClass;
+        this.health = health;
+    }
+
+    takeDamage(amount){
+        this.health -= amount;
+        console.log(`${this.name} took ${amount}damage! Health is now ${this.health}`);
+    }
+
+    
+
+
+}
+
+
+ const char_1 = new Character("thor", "ThunderGod", 100);
+ const char_2 = new Character("medusa", "unknown", 65);
+
+
+
+
+ console.log(char_1.heroClass);
+
+ console.log(char_1.health);
+  char_1.takeDamage(25);
+ console.log(char_2.health);
